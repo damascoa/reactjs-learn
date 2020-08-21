6c39a12aaae58928b56afba354e39434fd9d333d
Renato <contato.renatosi@hotmail.com>
2020-08-20 23:24:03 -0300

    Finalizando estudo basico de react

--- src/components/New/index.js
+++ src/components/New/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom'
+import firebase from '../../firebase'
 import './new.css'
 class New extends Component {
 
@@ -9,13 +10,35 @@ class New extends Component {
         this.state = {
             titulo: '',
             imagem: '',
-            descricao: ''
+            descricao: '',
+            alert: ''
         }
         this.cadastrar = this.cadastrar.bind(this);
     }
 
-    cadastrar() {
+    cadastrar = async (e) => {
+        console.log('clicou!')
+        e.preventDefault();
+        if (this.state.titulo.length > 0 && this.state.imagem.length > 0 && this.state.descricao.length > 0) {
+            let posts = firebase.app.ref('posts');
+            let chave = posts.push().key;
+            await posts.child(chave).set({
+                titulo: this.state.titulo,
+                image: this.state.imagem,
+                descricao: this.state.descricao,
+                autor: localStorage.nome
+            });
+            this.props.history.push('/dashboard');
+        } else {
+            this.setState({ alert: 'Preencha todos os campos' });
+        }
+    }
 
+    componentDidMount() {
+        if (!firebase.getCurrent()) {
+            this.props.history.replace("/");
+            return null;
+        }
     }
 
 
@@ -26,18 +49,16 @@ class New extends Component {
                     <Link to="/dashboard">Voltar</Link>
                 </header>
 
-                <form onsubmit={this.cadastrar()} id="newPost">
+                <form onSubmit={this.cadastrar} id="newPost">
+                    <span>{this.state.alert}</span>
                     <label>Título</label><br />
-                    <input type="text" placeholder="Nome do post" value={this.state.titulo}
-                        onChange={(e) => this.setState({ titulo: e.target.value })} /> <br />
+                    <input type="text" placeholder="Nome do post" value={this.state.titulo} onChange={(e) => this.setState({ titulo: e.target.value })} /> <br />
 
                     <label>URL Imagem</label><br />
-                    <input type="text" placeholder="URL da Imagem" value={this.state.imagem}
-                        onChange={(e) => this.setState({ imagem: e.target.value })} /> <br />
+                    <input type="text" placeholder="URL da Imagem" value={this.state.imagem} onChange={(e) => this.setState({ imagem: e.target.value })} /> <br />
 
                     <label>Descrição</label><br />
-                    <textArea type="text" placeholder="Descrição da Postagem" value={this.state.descricao}
-                        onChange={(e) => this.setState({ descricao: e.target.value })} /> <br />
+                    <textarea type="text" placeholder="Descrição da Postagem" value={this.state.descricao} onChange={(e) => this.setState({ descricao: e.target.value })} /> <br />
 
                     <button type="submit">Cadastrar</button>
 
